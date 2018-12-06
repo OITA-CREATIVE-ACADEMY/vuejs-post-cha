95784ef193ecc3ac6dd63cf3e99951f87c6669c0
TakuyaHarayama <harayama@ijgn.jp>
2018-12-06 19:40:06 +0900

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Post from '@/components/Post'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/post',
+      name: 'Post',
+      component: Post
+    },
+  ]
+})
