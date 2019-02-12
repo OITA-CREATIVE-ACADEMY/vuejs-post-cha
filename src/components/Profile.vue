a231d8702ab2e5cd00eb56ec39a4ff4377177f0d
yukikatsuragi <yuki@hab-co.jp>
2019-02-12 20:05:26 +0900

    ユーザーのアイコンをリストから変更する

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -8,59 +8,24 @@
           <b-btn v-b-modal.iconModal>Change!!</b-btn>
         </div>
         <!-- Modal Component -->
-        <b-modal id="iconModal" title="icon change!" size="lg" centered show="true">
+        <b-modal id="iconModal" title="icon change!" size="lg" centered show="true" ref="iconModalRef">
           <p class="my-4">アイコンを選んでください</p>
           <div class="iconList">
             <ul>
-              <!-- ここ！！！！！ -->
-              <!-- <li v-for="iconImage in iconImages" v-bind:key="">
-                {{ iconImages.src }}
-              </li> -->
               <li v-for="item in iconImages" v-bind:key="item.id">
-                <!-- <label for="{{ item.id }}"> -->
-                <img :src="item.src" alt="">
-                <p>{{ item.name }}</p>
-                <!-- <input type="radio" id="{{ item.id }}" name="iconImage"> -->
-              </li>
-              <li>
-                <label for="icon01">
-                <img :src="iconImages[0].src" alt="">
-                <p>{{iconImages[0].name}}</p></label>
-                <input type="radio" id="icon01" name="iconImage">
-              </li>
-              <li>
-                <label for="icon02">
-                <img :src="iconImages[1].src" alt="">
-                <p>{{iconImages[1].name}}</p></label>
-                <input type="radio" id="icon02" name="iconImage">
-              </li>
-              <li>
-                <div>
-                  <b-card title="Card Title"
-                          img-src="https://picsum.photos/600/300/?image=25"
-                          img-alt="Image"
-                          img-top
-                          tag="article"
-                          style="max-width: 20rem;"
-                          class="mb-2">
-                    <p class="card-text">
-                      Some quick example text to build on the card title and make up the bulk of the card's content.
-                    </p>
-                    <b-button href="#" variant="primary">Go somewhere</b-button>
-                  </b-card>
-                </div>
-                <!-- <label for="icon02">
-                <img :src="iconImages[1].src" alt="">
-                <p>{{iconImages[1].name}}</p></label>
-                <input type="radio" id="icon02" name="iconImage"> -->
+                <label :for="item.id">
+                  <img :src="item.src" alt="">
+                  <p>{{ item.name }}</p>
+                </label>
+                <input type="radio" :id="item.id" v-bind:value="item" v-model="iconSelected" name="iconSelect">
               </li>
             </ul>
           </div>
           <div slot="modal-footer" class="w-100">
-            <b-btn size="lg" variant="secondary" @click="show=false">
+            <b-btn size="lg" variant="secondary" @click="hideModal">
               Cansel
             </b-btn>
-            <b-btn size="lg" variant="primary"  v-on:click="changeIcon()">
+            <b-btn size="lg" variant="primary"  v-on:click="changeIcon()" @click="hideModal">
               OK!
             </b-btn>
           </div>
@@ -102,10 +67,11 @@ export default {
       value: {},
       icon: [],
       iconImage_src: [],
+      iconSelected: [],
       iconImages: [
         { name: 'てんとうむし', id: 'icon01', src: 'https://firebasestorage.googleapis.com/v0/b/post-cha.appspot.com/o/images%2Ficons%2Ficon01.jpg?alt=media&token=9c7581b8-75c8-49c2-973a-ae68d6b74042' },
         { name: 'スイカ', id: 'icon02', src: 'https://firebasestorage.googleapis.com/v0/b/post-cha.appspot.com/o/images%2Ficons%2Ficon02.jpg?alt=media&token=b732c5eb-be7c-498a-ac89-488d9f521d1f' },
-        { name: 'その他', id: 'icon03', src: 'https://firebasestorage.googleapis.com/v0/b/post-cha.appspot.com/o/images%2Ficons%2Ficon02.jpg?alt=media&token=b732c5eb-be7c-498a-ac89-488d9f521d1f' }
+        { name: 'いちご', id: 'icon03', src: 'https://firebasestorage.googleapis.com/v0/b/post-cha.appspot.com/o/images%2Ficons%2Ficon03.jpg?alt=media&token=e0d367bd-82f5-49cc-844e-0bf83ca75339' }
      ]
     }
   },
@@ -127,25 +93,23 @@ export default {
   //   }
   // },
   methods: {
+    showModal () {
+      this.$refs.iconModalRef.show()
+    },
+    hideModal () {
+      this.$refs.iconModalRef.hide()
+    },
     changeIcon: function () {
+      var selectedIcon = this.iconSelected.src;
+      console.log(selectedIcon);
+
       var user = firebase.auth().currentUser;
       var photoURL = user.photoURL;
-      console.log(user);
-      
-      var storage = firebase.storage();
-      var storageRef = storage.ref();
-      var imagesRef = storageRef.child('images');
-      var iconsRef = imagesRef.child('icons');
-      var icon01 = iconsRef.child('icon01.jpg');
-      var selectIcon = "https://firebasestorage.googleapis.com/v0/b/post-cha.appspot.com/o/images%2Ficons%2Ficon01.jpg?alt=media&token=9c7581b8-75c8-49c2-973a-ae68d6b74042";
-      // selectIconを動的に変更する、ループでiconImagesの中を回す
-
 
       user.updateProfile({
-         photoURL: selectIcon
+         photoURL: selectedIcon
       }).then(function() {
         // Update successful.
-        console.log(photoURL);
         alert("成功しました！");
         return;
       }).catch(function(error) {
