726c8b46169f615395c5b8203683596c0346f429
miyuki1130 <uki4024@gmail.com>
2019-02-12 18:48:03 +0900

    no message

--- src/App.vue
+++ src/App.vue
@@ -50,8 +50,8 @@
   </b-navbar>
 
   <!-- signin modal -->
-  <b-modal hide-footer id="modalPrevent"
-      ref="modal"
+  <b-modal hide-footer id="signin-modalPrevent"
+      ref="signinModal"
       title="ログイン">
     <form @submit.stop.prevent="handleSubmit">
     <b-form-input type="text" placeholder="メールアドレス" v-model="email"></b-form-input><br>
@@ -59,9 +59,35 @@
     </form>
     <p><button @click="signIn" type="button" class="btn btn-primary">ログイン</button></p>
     <p>アカウントをお持ちでない方はこちら 
-      <router-link to="/signup">新規登録!!</router-link>
+      <b-btn v-b-modal.signup-modalPrevent>新規登録!!</b-btn>
     </p>
   </b-modal>
+
+  <!-- signup modal -->
+  <b-modal hide-footer id="signup-modalPrevent"
+      ref="signupModal"
+      title="新規アカウント登録">
+    <form @submit.stop.prevent="handleSubmit">
+    <b-form-input type="text" placeholder="メールアドレス" v-model="email"></b-form-input><br>
+    <b-form-input type="password" placeholder="パスワード" v-model="password"></b-form-input><br>
+    </form>
+    <p><button @click="signUp" type="button" class="btn btn-primary">登録</button></p>
+    <p>既にアカウントをお持ちの方はこちら 
+      <b-btn v-b-modal.signin-modalPrevent>ログイン!!</b-btn>
+    </p>
+  </b-modal>
+
+  <!-- <template>
+  <div class="signup">
+    <h2>新規アカウント登録</h2>
+    <input type="text" placeholder="メールアドレス" v-model="email">
+    <input type="password" placeholder="パスワード" v-model="password">
+    <button @click="signUp" class="btn btn-success">登録</button>
+    <p>既にアカウントをお持ちの方はこちら 
+      <router-link to="/signin">ログイン!!</router-link>
+    </p>
+  </div>
+</template> -->
 
   <div id="app">
     <router-view/>
@@ -96,15 +122,34 @@ export default {
           console.log(res)
           alert('ログインしました!')
           this.$router.push('/')
+          console.log("テスト1")
+          this.hideModal()
         },
         err => {
           alert(err.message)
         }
       )
     },
-      hideModal() {
-      this.$refs.modal.hide();
+    signUp: function () {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+        .then(res => {
+          console.log(res)
+          alert('登録しました: ' + res.user.email)
+          this.$router.push('/')
+          this.hideModal()
+        })
+        .catch(error => {
+          alert(error.message)
+        })
     },
+    hideModal: function () {
+      console.log("テスト3");
+      console.log(this.$refs);
+
+      this.$refs.signinModal.hide();
+      this.$refs.signupModal.hide();
+    },
+
     signOut: function () {
       firebase.auth().signOut().then(() => {
         this.$router.push('/')
